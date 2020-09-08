f7f244bf9306965820690442620da08821f03312
mplatoc <70793386+mplatoc@users.noreply.github.com>
2020-09-07 23:12:02 -0600

    nuevo

---
+++ GIT/GIT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GIT.Global" Language="C#" %>
---
+++ GIT/GIT/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GIT.Startup))]
+namespace GIT
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
